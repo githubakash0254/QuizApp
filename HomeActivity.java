f4f0f9dea04bfbb07cb98fc791fdc0196ba2ddd4
Muskan garg <37190754+muskangrg96@users.noreply.github.com>
2019-08-18 02:01:49 -0400

    about code
    this code is working fine now

--- HomeActivity.java
+++ HomeActivity.java
@@ -13,7 +13,6 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.MenuItem;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
-
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
